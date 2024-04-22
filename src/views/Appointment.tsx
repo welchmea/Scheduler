6ebd07dae44354090add5956c6ee24956721e11f
Meagan <welchmea@oregonstate.edu>
2024-04-22 18:11:51 -0500

    retrieved data from appointment page for db

--- src/views/Appointment.tsx
+++ src/views/Appointment.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import Calendar from "react-calendar";
 import { useNavigate } from "react-router-dom";
 
 function Appointment() {
   const navigate = useNavigate();
-
+  const [date, setDate] = useState('');
   const options = [
     "Cut",
     "Color",
@@ -17,58 +18,60 @@ function Appointment() {
   const times = ["9:00 am", "10:00 am", "12:00 pm", "1:00 pm"];
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    console.log("here")
+
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    console.log(data)
     console.log({
-      name: data.get("name"),
-      day: data.get("day"),
+      service: data.get("service"),
       date: data.get("date"),
       time: data.get("time"),
       description: data.get("description"),
     });
 
-    const newAppt = {
-      name: data.get("name"),
-      day: data.get("day"),
-      date: data.get("date"),
-      time: data.get("time"),
-      description: data.get("description"),
-    };
-    const results = await fetch("http://localhost:5000/createAppointment", {
-      method: "post",
-      body: JSON.stringify(newAppt),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    // const newAppt = {
+    //   service: data.get("service"),
+    //   date: data.get("date"),
+    //   time: data.get("time"),
+    //   description: data.get("description"),
+    // };
+    // const results = await fetch("http://localhost:5000/createAppointment", {
+    //   method: "post",
+    //   body: JSON.stringify(newAppt),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
 
-    if (results.status === 201) {
-      alert(`Your appointment is set!`);
-      navigate("/");
-    } else {
-      alert(
-        `Defer to the status code: ${results.status}, to determine what went wrong.`
-      );
-    }
-    navigate("/");
+    // if (results.status === 201) {
+    //   alert(`Your appointment is set!`);
+    //   navigate("/");
+    // } else {
+    //   alert(
+    //     `Defer to the status code: ${results.status}, to determine what went wrong.`
+    //   );
+    // }
+    // navigate("/");
   };
 
   return (
     <>
       <div className="flex flex-wrap p-2 gap-x-4 justify-center items-start z-2 absolute mt-48 text-black w-full">
-        <form
-        >
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-1 border border-black rounded-md h-5/6 p-8 bg-white">
-            <label className="flex text-xl border border-black bg-calendarBG p-4 rounded-md">
+            <label id="date" className="flex text-xl border border-black bg-calendarBG p-4 rounded-md">
               Appointment Scheduler
-            </label>
+            
             <div
               id="date"
               className="flex flex-wrap border border-black rounded-md"
             >
-              <Calendar />
+              <Calendar onClickDay={(data) => {
+                console.log(data.getDate(), data.getMonth());
+                // setDate(data);
+              }} />
             </div>
+            </label>
             <div className="flex text-xl border border-black bg-calendarBG p-4 rounded-md">
               Stylist: Melissa McGill
             </div>
@@ -78,17 +81,18 @@ function Appointment() {
             <div className="flex flex-col place-content-center gap-y-1">
              
              <div className="flex text-xl border border-black bg-calendarBG p-4 rounded-md items-center">
-                <label className="flex mr-12" id="day">Select one option</label>
-                <select name="day" className="bg-white p-1 border rounded-md w-[30vh]">
+                <label className="flex mr-12" id="time">Select one option
+                <select name="service" className="bg-white p-1 border rounded-md w-[30vh]">
                   {
                     options.map((i) => (
                       <option key={i} value={i}>{i}</option>
                     ))
                   }
                 </select>
+                </label>
               </div>
               <div className="flex text-xl border border-black bg-calendarBG p-4 rounded-md items-center">
-                <label className="flex mr-12" id="time">Select one option</label>
+                <label className="flex mr-12" id="time">Select one option
                 <select name="time" className="bg-white p-1 border rounded-md w-[30vh]">
                   {
                     times.map((i) => (
@@ -96,6 +100,7 @@ function Appointment() {
                     ))
                   }
                 </select>
+                </label>
               </div>
               {/* <div className="flex flex-col items-start text-xl border border-black bg-calendarBG p-4 rounded-md gap-y-4">
                 <label id="time">Pick a time:</label>
@@ -116,16 +121,18 @@ function Appointment() {
                   className="flex text-xl border bg-white border-black p-3"
                 >
                   Notes for stylist...
-                </label>
+                
                 <textarea
                   name="description"
                   id="description"
+
                   className="flex bg-white border border-black w-[60vh] h-[21vh] p-3"
                 ></textarea>
+                </label>
               </div>
               <div className="flex border border-black p-3 text-md bg-calendarBG shadow-lg rounded-md">
                 <button
-                  onSubmit={()=>handleSubmit}
+                  
                   type="submit"
                   className="hover:bg-backgroundCard bg-white transition delay-100 duration-300 ease-in-out transform hover:scale-105 text-xl w-1/3 border border-black p-2 rounded-md"
                 >

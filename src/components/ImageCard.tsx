caf1fa72cbedcc9e31ea593ca2267a8aa8f4bc09
Meagan <welchmea@oregonstate.edu>
2024-05-14 16:34:25 -0500

    removed images sizes

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,10 +1,10 @@
-export default function ImageCard({ title, img, imageSmall}: { title: String; img: string, imageSmall: string}) {
+export default function ImageCard({ title, img}: { title: String; img: string}) {
   return (
     <>
       <div className="bg-black rounded-md p-2 w-1/4 transition duration-300 ease-in-out transform hover:scale-105">
         {title}
         <img
-          srcSet={`${imageSmall} 320w, ${img} 1980w`}
+          // srcSet={`${imageSmall} 320w, ${img} 1980w`}
           src={img}
           width="400"
           height="600"

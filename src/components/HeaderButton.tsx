a9da877f34e857ed710b6912b3a9b438f66dba16
Meagan <welchmea@oregonstate.edu>
2024-06-13 17:24:25 -0500

    improving performance

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -2,7 +2,7 @@ export default function HeaderButtonStyle({ title }: { title: String }) {
   return (
     <>
       <div className="flex border border-black rounded-sm hover:bg-calendarBG p-2 transition delay-50 duration-300 ease-in-out transform hover:scale-110">
-        <button>{title}</button>
+        <div>{title}</div>
       </div>
     </>
   );

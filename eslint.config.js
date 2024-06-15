0ac1caf2b75200840ffd4546f27bdd4335e112ce
Meagan <welchmea@oregonstate.edu>
2024-06-14 19:02:35 -0500

    changed linting call in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  pluginReactConfig,
+  ...fixupConfigRules(pluginReactConfig),
 ];

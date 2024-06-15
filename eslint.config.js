24e29f977c57a1d87456ae31f0cbd9323d8af2da
Meagan <welchmea@oregonstate.edu>
2024-06-15 12:43:57 -0500

    disables console error

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "off",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+          message: "Unexpected property on console object was called",
+        },
+      ],
+    },
+  }
 );

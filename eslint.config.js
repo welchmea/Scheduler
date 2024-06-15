f610e70a2ef84069c7de59b6758be4ec0ec0c9de
Meagan <welchmea@oregonstate.edu>
2024-06-15 13:01:06 -0500

    removed linter, not approved Qodana version, and modified rule in eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -9,14 +9,7 @@ export default tseslint.config(
   {
     rules: {
       "no-console": "off",
-      "no-restricted-syntax": [
-        "error",
-        {
-          selector:
-            "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-          message: "Unexpected property on console object was called",
-        },
-      ],
+      "no-alert": "off"
     },
   }
 );
